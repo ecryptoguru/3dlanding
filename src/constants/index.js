f840249fbe7d0602ad78e0966873b2aff92e4f3f
Ankit Das <ecryptoguru@gmail.com>
2023-11-16 15:37:49 +0530

    update

---
+++ src/constants/index.js
@@ -0,0 +1,19 @@
+import { contact, github, linkedin } from "../assets/icons";
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/YourGitHubUsername",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/YourLinkedInUsername",
+  },
+];
