fe5dd7e47a54d48f3398497642fa71eff281fde5
Ankit Das <ecryptoguru@gmail.com>
2023-11-26 20:19:23 +0530

    update

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,15 @@
 //import CTA from "./CTA";
 //import Alert from "./Alert";
-//import Loader from "./Loader";
+import Loader from "./Loader";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-//import HomeInfo from "./HomeInfo";
+import HomeInfo from "./HomeInfo";
 
 export {
   //CTA,
   //Alert,
-  //Loader,
+  Loader,
   Navbar,
   Footer,
-  //HomeInfo
+  HomeInfo,
 };
