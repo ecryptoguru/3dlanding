bc7ec5e28789044471038ab2ba03ab94bf85ff84
Ankit Das <ecryptoguru@gmail.com>
2023-12-01 02:21:39 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,16 +12,20 @@ const Navbar = () => {
         <NavLink
           to="https://nft.fusionwaveai.com/"
           target="_blank"
-          className="text-black"
+          className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
         >
-          FusionwaveNFT
+          <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            FusionwaveNFT
+          </span>
         </NavLink>
         <NavLink
           to="https://game.fusionwaveai.com/"
           target="_blank"
-          className="text-black"
+          className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
         >
-          FusionFight
+          <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            FusionFight
+          </span>
         </NavLink>
       </nav>
     </header>
