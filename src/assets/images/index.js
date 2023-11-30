9f04051338576cc642b5a553584a02e69082a562
Ankit Das <ecryptoguru@gmail.com>
2023-11-30 15:18:55 +0530

    final

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,8 +1,3 @@
-import hero from "./hero.jpg";
-import meta from "./meta.png";
-import shopify from "./shopify.png";
-import starbucks from "./starbucks.png";
-import tesla from "./tesla.png";
 import logo from "./logo.png";
 
-export { hero, meta, shopify, starbucks, tesla, logo };
+export { logo };
